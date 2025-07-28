0d8a50e02edcde1ce5312666548f1cdb2735d0e7
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-28 19:44:38 +0530

    Updated server

--- backend/prep-ai/server.js
+++ backend/prep-ai/server.js
@@ -22,7 +22,6 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://prepwise-a.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
