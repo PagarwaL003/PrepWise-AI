6eb8fd87957330a33d097d168f2d6c5e336971a8
Piyush Agarwal <agpiyush162003@gmail.com>
2025-07-28 19:53:29 +0530

    final commit

--- backend/prep-ai/server.js
+++ backend/prep-ai/server.js
@@ -21,9 +21,10 @@ const app = express();
   // middleware to handle cors
 app.use(
   cors({
-    origin: ["https://prepwise-a.vercel.app"],
+    origin: "https://prepwise-a.vercel.app",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
